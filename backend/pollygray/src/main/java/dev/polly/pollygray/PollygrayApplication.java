f8068c497f615805876493d94b90d4d34b9ec67e
Belarmino Simão Júnior <belarminosimaojunior@gmail.com>
2024-05-06 11:41:35 +0200

    Initial commit

---
+++ backend/pollygray/src/main/java/dev/polly/pollygray/PollygrayApplication.java
@@ -0,0 +1,13 @@
+package dev.polly.pollygray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollygrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollygrayApplication.class, args);
+	}
+
+}
---
+++ backend/pollygray/src/test/java/dev/polly/pollygray/PollygrayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.polly.pollygray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollygrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
