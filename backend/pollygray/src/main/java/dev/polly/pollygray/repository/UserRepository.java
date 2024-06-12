40fde7ecd535c00bf7c47305aaac4cf6b7d835f5
Nuno Fonseca Florencio <nunofonsecaflorencio@gmail.com>
2024-06-12 20:06:17 +0200

    fix: typo error

--- backend/pollygray/src/main/java/dev/polly/pollygray/repository/UserRepository.java
+++ backend/pollygray/src/main/java/dev/polly/pollygray/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository  extends MongoRepository<User, ObjectId> {
-    User findByUsername(String username);
+    User findByUserName(String username);
 }
