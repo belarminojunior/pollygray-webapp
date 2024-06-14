9e38f7eaed0c16f8e593ea6f1d6c2335082fba55
Belarmino Simão Júnior <belarminosimaojunior@gmail.com>
2024-06-14 14:05:43 +0200

    style(frontend): Signup Page

--- frontend/pollygray/src/pages/SignUp/SignUp.js
+++ frontend/pollygray/src/pages/SignUp/SignUp.js
@@ -13,69 +13,94 @@ const SignUp = () => {
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      authService.register()
+      authService.register();
       navigate("/login");
     } catch (error) {
-      alert('Error during registration')
+      alert("Error during registration");
     }
   };
 
   return (
-    <div>
-      <h2>Sign Up</h2>
-      <form action="" onSubmit={handleSignUp}>
-        <div>
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            type="text"
-            name="firstName"
-            id="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-        </div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="bg-white p-10 rounded-lg shadow-lg w-96">
+        <h2 className="text-2xl font-bold mb-5 text-center">Sign Up</h2>
+        <h3 className="text-lg font-semibold mb-5 text-center">
+          Register Here
+        </h3>
+        <form action="" onSubmit={handleSignUp} className="space-y-4">
+          <div>
+            <label htmlFor="firstName" className="block mb-1 font-semibold">
+              First Name:
+            </label>
+            <input
+              className="w-full px-4 py-2 border rounded-lg"
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          </div>
 
-        <div>
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            type="text"
-            name="lastName"
-            id="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </div>
+          <div>
+            <label htmlFor="lastName" className="block mb-1 font-semibold">
+              Last Name:
+            </label>
+            <input
+              className="w-full px-4 py-2 border rounded-lg"
+              type="text"
+              name="lastName"
+              id="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </div>
 
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
+          <div>
+            <label htmlFor="email" className="block mb-1 font-semibold">
+              Email:
+            </label>
+            <input
+              className="w-full px-4 py-2 border rounded-lg"
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
 
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
-      <p>
-        Already have an account? <a href="/login">Login</a>
-      </p>
+          <div>
+            <label htmlFor="password" className="block mb-1 font-semibold">
+              Password:
+            </label>
+            <input
+              className="w-full px-4 py-2 border rounded-lg"
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            className="w-full bg-black text-white py-2 rounded-lg font-semibold"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </form>
+        <p className="mt-4 text-center">
+          Already have an account?{" "}
+          <a href="/login" className="text-blue-500">
+            Login
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
